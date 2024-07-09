76fb3da1a82e40c78b90d6e8f7e817e9eb45fc66
Dawood Aijaz <dawoodaijaz.esketchers@gmail.com>
2024-07-09 12:11:44 +0200

    1.Implement https://app.asana.com/0/445384129536859/1207733378229060

---
+++ .changeset/small-drinks-taste.md
@@ -0,0 +1,5 @@
+---
+"cm2-worker": minor
+---
+
+Add EMPLOYEE_RECORD project type
--- src/constants/project.constants.ts
+++ src/constants/project.constants.ts
@@ -1,12 +1,13 @@
-export enum ProjectTypes{
-    regular="REGULAR",
-    inbox="INBOX",
-    outbox="OUTBOX",
-    messageTemplate="MESSAGE_TEMPLATE",
+export enum ProjectTypes {
+    regular = "REGULAR",
+    inbox = "INBOX",
+    outbox = "OUTBOX",
+    messageTemplate = "MESSAGE_TEMPLATE",
     loveLetterMessages = "LOVE_LETTER_MESSAGES",
     cloudmate = "CLOUDMATE",
     draft = "DRAFT",
-    timeLogging="TIME_LOGGING",
-    employees="EMPLOYEES",
+    timeLogging = "TIME_LOGGING",
+    employeesRecord = "EMPLOYEES_RECORD",
+    employee = "EMPLOYEE",
 }
 
